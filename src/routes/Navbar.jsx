803c6e17f73130936566757e536b832dab7bccf4
citraFebriawirti <febriawirticitra@gmail.com>
2023-01-17 23:21:33 +0700

    aaacccddd

--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar() {
               className="hidden w-full md:block md:w-auto"
               id="navbar-default"
             >
-              <ul className="flex flex-col p-4 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <ul className="flex flex-col p-4 mt-4 border rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0  dark:border-gray-700">
                 <li>
                   <Link
                     to="/"
