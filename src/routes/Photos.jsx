14e6cf2638b4efd9100bdf9f34d6e45a4654e8d7
citraFebriawirti <febriawirticitra@gmail.com>
2023-01-17 23:35:55 +0700

    ketigaa

--- src/routes/Photos.jsx
+++ src/routes/Photos.jsx
@@ -68,7 +68,7 @@ const Photos = () => {
           <select
             onChange={(e) => setSort(e.target.value)}
             data-testid="sort"
-            className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  mr-12 lg:mr-0 lg:w-32"
+            className="bg-gray-50 border border-gray-300  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-20 p-2.5  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  mr-12 lg:mr-0 lg:w-32"
             style={{}}>
             <option value="asc">Ascending</option>
             <option value="desc">Descending</option>
@@ -84,7 +84,7 @@ const Photos = () => {
               data-testid="search"
               onChange={(e) => setSearch(e.target.value)}
               className=" self-center bg-gray-50 border border-gray-300 
-               text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-25 p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  md:ml-10"/>
+               text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-25 p-2 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500  md:ml-10"/>
            
             <button type="submit" value="Search" data-testid="submit" class="p-2.5 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ">
              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
